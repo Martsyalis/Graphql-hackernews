71213069b7cbf416cb9c53d05cbf05c77c58c3b1
Maryus Martsyalis <martsyalis@gmail.com>
2019-07-24 12:43:09 -0700

    formating

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-let idCount = links.length
+let links = [
+  {
+    id: "link-01",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+let idCount = links.length;
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (parent, { id }) => links.find(link => link.id === id)
   },
+
   Mutation: {
     post: (parent, args) => {
-       const link = {
+      const link = {
         id: `link-${idCount++}`,
         description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    update: (parent, args) => {
+      console.log("args are", args);
+      const i = links.findIndex(link => link.id === args.id);
+      links[i] = args;
+      return links[i];
     }
-  },
-}
+  }
+};
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
