3a1b0462501646c636a5de068b92ce51a4e1150f
Maryus Martsyalis <martsyalis@gmail.com>
2019-07-25 16:16:13 -0700

    adds login and sign up resolvers and utils for auth

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,20 +1,43 @@
-import { buildSchemaFromTypeDefinitions } from "graphql-tools";
-import { isError } from "util";
-
-async function signup(root, args, context, info) {
+async function signup(parent, args, context, info) {
+  // 1
   const password = await bcrypt.hash(args.password, 10);
+  // 2
   const user = await context.prisma.createUser({ ...args, password });
-  const token = jwt.sign({ userId: isError.id }, APP_SECRET);
 
+  // 3
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  // 4
   return {
     token,
     user
   };
 }
 
-async function login(root, args, context, info) {
-    const user = await context.prisma.user({ email: args.email })
-    if(!user) {
-        throw new Error('No such user found')
-    }
+async function login(parent, args, context, info) {
+  // 1
+  const user = await context.prisma.user({ email: args.email });
+  if (!user) {
+    throw new Error("No such user found");
+  }
+
+  // 2
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("Invalid password");
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  // 3
+  return {
+    token,
+    user
+  };
 }
+
+module.exports = {
+  signup,
+  login,
+  post
+};
